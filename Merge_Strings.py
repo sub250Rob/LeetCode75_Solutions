3da9a7a00308b98314a75abd852875e018976876
Robby Wandel <robbywandel@Robbys-MacBook-Pro.local>
2026-01-04 09:10:56 -0800

    First Commit with README and first solution.

---
+++ Merge_Strings.py
@@ -0,0 +1,25 @@
+
+
+class Solution:
+
+    def __init__(self):
+        pass
+
+    def mergeAlternately(self, word1, word2):
+
+        merged = ""
+
+        if len (word1) < len(word2):
+            smaller = word1
+            other = word2
+        else:
+            smaller = word2
+            other = word1
+        
+        for i in range (len(smaller)): 
+            merged += word1[i]
+            merged += word2[i]
+        
+        merged += other[(len(smaller)):]
+
+        return merged
---
+++ README.md
@@ -0,0 +1,1 @@
+# LeetCode75_Solutions (Python Only)
